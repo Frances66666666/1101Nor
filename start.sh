eedc75fc66dfa270d149b86d76fcc3a892459f98
Frances66666666 <106012020+Frances66666666@users.noreply.github.com>
2022-11-01 01:23:30 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 PORT=43011
 UUID=f79c1d06-e1a2-4136-be4e-1d9a86c7e69d
 WebPage=https://baidu.com
-CaddyConfig=https://raw.githubusercontent.com/chaohuxiangxiang/python/main/etc/Caddyfile
-XRayConfig=https://raw.githubusercontent.com/chaohuxiangxiang/python/main/etc/xray.json
+CaddyConfig=https://raw.githubusercontent.com/Frances66666666/1101Nor/main/etc/Caddyfile
+XRayConfig=https://raw.githubusercontent.com/Frances66666666/1101Nor/main/etc/xray.json
 Xray_Newv=`wget --no-check-certificate -qO- https://api.github.com/repos/XTLS/Xray-core/tags | grep 'name' | cut -d\" -f4 | head -1 | cut -b 2-`
 # Install XRay
 mkdir -p /tmp/app
